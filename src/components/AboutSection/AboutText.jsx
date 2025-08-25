24f04f53cccf8a0ce9c67907b67bb94580be0982
Eswar <eswarkathir66@gmail.com>
2025-08-25 10:38:15 +0530

    Added resume

--- src/components/AboutSection/AboutText.jsx
+++ src/components/AboutSection/AboutText.jsx
@@ -8,9 +8,22 @@ const AboutText = () => {
     <div className='flex flex-col md:items-start sm:items-center md:text-left sm:text-center '>
         <h1 className='text-6xl uppercase text-cyan mb-10'>About Me</h1>
         <p className='text-white '>Hi I'm Eswar.! I'm a passionate and detail-oriented Full Stack Developer with a strong foundation in both front-end and back-end technologies. I specialize in building dynamic, responsive web applications that deliver seamless user experiences.With a solid grasp of HTML, CSS, and JavaScript, I craft clean, user-friendly interfaces using React.js and modern styling tools like Tailwind CSS. On the backend, I develop robust APIs and server-side logic with Node.js, Express.js, and Java.I enjoy turning ideas into reality through code, and I’m always eager to learn new technologies, solve complex problems, and collaborate on meaningful projects.</p>
-        <Link to="projects" smooth={true} spy={true} duration={500} offset={-130} className='cursor-pointer text-white hover:text-cyan transition-all duration-500'>
-         <button className='bg-amber-400 border-4 border-white rounded-full mt-5 py-2 px-4 cursor-pointer hover:bg-amber-300 active:bg-amber-600 transition-all duration-400 md:self-start sm:self-center'>My Projects</button>
-        </Link>
+        <div className="flex gap-4 justify-center items-center">
+          <Link to="projects" smooth={true} spy={true} duration={500} offset={-130} className='cursor-pointer text-white hover:text-cyan transition-all duration-500'>
+            <button className='bg-amber-400 border-4 border-white rounded-full mt-5 py-2 px-4 cursor-pointer hover:bg-amber-300 active:bg-amber-600 transition-all duration-400 md:self-start sm:self-center'>My Projects</button>
+          </Link>
+        
+          <a
+            href="/Eswar's_Resume.pdf" 
+            download
+            className='ursor-pointer text-white hover:text-cyan transition-all duration-500'
+          >
+            <button className='bg-cyan-400 border-4 border-white rounded-full mt-5 py-2 px-4 cursor-pointer hover:bg-amber-300 active:bg-amber-600 transition-all duration-400 md:self-start sm:self-center'>
+              Download Resume
+            </button>
+          </a>
+        </div>
+       
        
     </div>
   )
--- src/components/Navbar/NavbarBtn.jsx
+++ src/components/Navbar/NavbarBtn.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { LuArrowDownRight } from 'react-icons/lu'
+import { Link } from 'react-scroll'
 
 const NavbarBtn = () => {
   return (
+    <Link to='contact'>
     <button className='px-4 py-2 rounded-full text-xl font-bold border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange hover:scale-110 transistion-all duration-400 cursor-pointer hover:border-orange hover:shadow-cyanShadow text-white'>
       Hire me <LuArrowDownRight/>
     </button>
+    </Link>
   )
 }
 
